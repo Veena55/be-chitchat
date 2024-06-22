6262f2f59388fe43fc3476d3c3ef6b3dbce38329
Veena Rao <veena.500apps@gmail.com>
2024-06-22 15:20:08 +0530

    protected-routes continue-with-google

--- routes/auth.js
+++ routes/auth.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const authController = require('../controllers/AuthController');
+const { authenticate } = require('../middlewares/auth');
 
-router.get('/', authController.verifyUser);
-router.post('/verify_google_token', authController.verifyGoogleToken);
+router.get('/', authenticate, authController.verifyUser);
+
+router.post('/verify_google_token', authController.continueWithGoogle);
 
 
 module.exports = router;
