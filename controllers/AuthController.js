870a595eb2d30ed34c20312e58691257abdc1b85
Veena Rao <veena.500apps@gmail.com>
2024-06-22 15:20:08 +0530

    protected-routes continue-with-google

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -1,5 +1,7 @@
 const { OAuth2Client } = require('google-auth-library');
 const jwtController = require('../controllers/JwtTokenController');
+const { verifyGoogleToken } = require('../middlewares/auth');
+const { isTokenPresent } = require('../utils/token');
 require('dotenv').config();
 
 const client = new OAuth2Client(process.env.GOOGLE_CLIENT_ID);
@@ -8,24 +10,20 @@ const verifyUser = (req, res, next) => {
     console.log("Hi");
     return res.send("Verified");
 }
-
-const verifyGoogleToken = async (req, res) => {
-    const { token } = req.body;
+const continueWithGoogle = async (req, res) => {
     try {
-        // Verify the ID token
-        const ticket = await client.verifyIdToken({
-            idToken: token,
-            audience: process.env.GOOGLE_CLIENT_ID
-        });
-        const payload = ticket.getPayload();
-        const { sub, email, user } = payload;
+        const token = isTokenPresent;
+        if (token) {
+            // Verify the ID token        
+            const payload = verifyGoogleToken(req, res);
+            const { sub, email, user } = payload;
 
-        //generate JwtToken
-        const jwtToken = jwtController.generateJwtToken(payload, process.env.JWT_SECRET_KEY);
+            // generate JwtToken
+            const jwtToken = jwtController.generateJwtToken(payload, process.env.JWT_SECRET_KEY);
 
-        // If the user is verified, return the user data
-        return res.status(200).json({ token: jwtToken });
-
+            // If the user is verified, return the user data
+            return res.status(200).json({ token: jwtToken });
+        }
     } catch (error) {
         return res.status(401).json({ "msg": "Invalid Token" });
     }
@@ -35,5 +33,5 @@ const verifyGoogleToken = async (req, res) => {
 
 module.exports = {
     verifyUser,
-    verifyGoogleToken
+    continueWithGoogle
 }
---
+++ middlewares/auth.js
@@ -0,0 +1,42 @@
+const jwtController = require('../controllers/JwtTokenController');
+const { isTokenPresent } = require('../utils/token');
+
+const verifyGoogleToken = async (req, res, token) => {
+    try {
+        const ticket = await client.verifyIdToken({
+            idToken: token,
+            audience: process.env.GOOGLE_CLIENT_ID
+        });
+        const payload = ticket.getPayload();
+        return payload;
+    } catch (error) {
+        return res.status(401).json({ message: "Invalid Token" });
+    }
+}
+
+
+const authenticate = async (req, res, next) => {
+    const token = isTokenPresent(req, res);
+    if (token) {
+        const decodeToken = jwtController.decodeJwtToken(token);
+        if (!decodeToken) return res.status(401).json({ message: 'Invalid Token!!' });
+        if (decodeToken.header.alg.startsWith('RS')) {
+            const payload = await verifyGoogleToken(decodeToken);
+            req.user = payload;
+            next();
+        } else {
+            const jwtToken = jwtController.verifyJwtToken(decodeToken);
+            if (jwtToken) {
+                req.user = jwtToken;
+                next();
+            }
+        }
+    }
+}
+
+
+
+module.exports = {
+    authenticate,
+    verifyGoogleToken
+}
