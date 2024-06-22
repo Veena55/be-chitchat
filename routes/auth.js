efd3398398529a47f19615f73f303b66d88b6135
Veena Rao <veena.500apps@gmail.com>
2024-06-22 19:59:05 +0530

    db_setup, user chema created

--- routes/auth.js
+++ routes/auth.js
@@ -1,11 +1,18 @@
 const express = require('express');
+const rateLimit = require('express-rate-limit');
 const router = express.Router();
 const authController = require('../controllers/AuthController');
 const { authenticate } = require('../middlewares/auth');
 
-router.get('/', authenticate, authController.verifyUser);
+const specificLimiter = rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 10, // Limit each IP to 10 requests per windowMs
+    message: 'Too many requests from this IP, please try again after 15 minutes'
+});
 
-router.post('/verify_google_token', authController.continueWithGoogle);
+router.get('/', specificLimiter, authenticate, authController.verifyUser);
+
+router.post('/verify_google_token', specificLimiter, authController.continueWithGoogle);
 
 
 module.exports = router;
