e95dd976de631fbda99bd308809cd441cc0f6e83
VIJAY SHANKAR VISHWAKARMA <avshankar9151@gmail.com>
2024-07-04 13:54:17 +0530

    friend-list schema

---
+++ models/Friend.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+
+const friendSchema = new mongoose({
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true,
+    },
+    friend: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true,
+    },
+    accepted: {
+        type: Boolean,
+        default: false,
+    },
+}, {
+    timestamps: {
+        createdAt: 'created_at',
+        updatedAt: 'updated_at',
+    }
+});
+
+const Friend = mongoose.model('friend', friendSchema);
+
+module.exports = Friend;
